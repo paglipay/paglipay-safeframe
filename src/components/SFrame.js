6becbbad937e7046176c39ae5d89a76ceb32a17b
paglipay <paglipay@gmail.com>
2022-08-12 13:23:13 -0700

    paglipay.github.io/iframe-post-messages/

--- src/components/SFrame.js
+++ src/components/SFrame.js
@@ -6,7 +6,7 @@ export const SFrame = forwardRef(({ id }, forwardedRef) => {
       ref={forwardedRef}
       title={id}
       id={id}
-      src="http://localhost:5500/frame3.htm"
+      src="https://paglipay.github.io/iframe-post-messages/frame3.htm"
       frameBorder="1"
       style={{ display: "block", height: "200px", border: "1px solid black" }}
     ></iframe>
